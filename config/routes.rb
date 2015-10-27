1b39bb743f6b4e19b0a84655016834472a8ddbaf
Jade Feng <jade.jiefeng@gmail.com>
2015-10-28 01:59:30 +1100

    remove Contact controller and model

--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,15 @@ Rails.application.routes.draw do
   resources :projects
   resources :genres
   resources :categories
+  
   get 'pages/about'
   
   # devise_for :users
   devise_for :users, controllers: { registrations: "registrations" }
   resources :users
 
-  match '/contacts',     to: 'contacts#new',             via: 'get'
-  resources "contacts", only: [:new, :create]
+  # match '/contacts',     to: 'contacts#new',             via: 'get'
+  # resources "contacts", only: [:new, :create]
 
   root :to => "pages#index"
 
